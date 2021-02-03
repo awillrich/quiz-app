3f2bd2146d0c0e720aebc601fe96337a1446d742
André Willrich <andre@willrich.info>
2021-02-03 15:40:32 +0100

    Antworten ein- und ausblenden mit foreach

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 console.clear;
 console.log('Beginn')
 
-const button1 = document.querySelector('#answer_button1')
-const answer1 = document.querySelector('#answer1')
+const buttons = document.querySelectorAll('.answer_button')
 
-console.log(answer1)
-
-button1.addEventListener('click', () => {
-    answer1.classList.toggle('hidden')
-    console.log('toggle')
-});
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentNode.querySelector('.right_answer').classList.toggle('hidden')
+    })
+    
+    
+}) 
