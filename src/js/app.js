bff2a9e2936df2f554d0436a96a1b04d8b91b97c
André Willrich <andre@willrich.info>
2021-02-03 17:17:02 +0100

    Bookmark + Answer anklickbar

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,23 @@
 console.clear;
 
-const buttons = document.querySelectorAll('.answer_button')
+const answer_buttons = document.querySelectorAll('.answer_button')
+const answers = document.querySelectorAll('.answer')
+const bookmarks = document.querySelectorAll('.bookmark')
 
-buttons.forEach(button => {
+answer_buttons.forEach(button => {
     button.addEventListener('click', () => {
         button.parentNode.querySelector('.right_answer').classList.toggle('hidden')
+    })
+}) 
+
+answers.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('marked')
+    })
+}) 
+
+bookmarks.forEach(sign => {
+    sign.addEventListener('click', () => {
+        sign.classList.toggle('bookmark__active')
     })
 }) 
